4619583c18d5687a5a7e885710fbc708b30c722d
Cintia Ribeiro <cintiarbrandao@gmail.com>
2020-08-11 12:18:53 -0300

    testando

--- tests/Feature/Http/Controllers/VideoController/VideoControllerUploadsTest.php
+++ tests/Feature/Http/Controllers/VideoController/VideoControllerUploadsTest.php
@@ -13,14 +13,14 @@ class VideoControllerUploadsTest extends BaseVideoControllerTestCase
 {
     use TestValidation, TestUploads;
 
-    public function testInvalidationVideoField()
-    {
-        $this->assertInvalidationFile (
-            'video_file',
-            'mp4',
-            video::VIDEO_FILE_MAX_SIZE,
-            'mimetypes', ['values' => 'video/mp4']);
-    }
+    // public function testInvalidationVideoField()
+    // {
+    //     $this->assertInvalidationFile (
+    //         'video_file',
+    //         'mp4',
+    //         video::VIDEO_FILE_MAX_SIZE,
+    //         'mimetypes', ['values' => 'video/mp4']);
+    // }
 
     // public function testInvalidationThumberFile()
     // {
